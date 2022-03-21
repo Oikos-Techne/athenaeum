f104ac5ab2ffb6419550f66af501363755018d09
kburton501 <kburton501@gmail.com>
2022-03-20 23:35:34 -0700

    connected book info and review microservices

--- athenaeum-catalogue-service/src/main/java/dev/kellyburton/athenaeumcatalogueservice/resources/AthenaeumCatalogResource.java
+++ athenaeum-catalogue-service/src/main/java/dev/kellyburton/athenaeumcatalogueservice/resources/AthenaeumCatalogResource.java
@@ -1,22 +1,39 @@
 package dev.kellyburton.athenaeumcatalogueservice.resources;
 
+import dev.kellyburton.athenaeumcatalogueservice.models.Book;
 import dev.kellyburton.athenaeumcatalogueservice.models.CatalogItem;
+import dev.kellyburton.athenaeumcatalogueservice.models.Rating;
+import dev.kellyburton.athenaeumcatalogueservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
+import java.lang.reflect.ParameterizedType;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalogue")
 public class AthenaeumCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{UUID}")
     public List<CatalogItem> getCatalog(@PathVariable("UUID") String UUID) {
 
-        return Collections.singletonList(
-                new CatalogItem("Aliens", "chest bursting fun", 5)
-        );
+
+        UserRating ratings = restTemplate.getForObject("http://localhost:8082/ratingsdata/users/" +UUID, UserRating.class);
+
+        return ratings.getUserRating().stream().map(rating -> {
+                    // Iterating through each book id, call book info service
+                    Book book = restTemplate.getForObject("http://localhost:8081/books/" + rating.getBookId(), Book.class);
+                    return new CatalogItem(book.getName(), "Chestbursting fun", rating.getRating());
+                })
+                .collect(Collectors.toList());
+
     }
 }
--- athenaeum-ratings-service/src/main/java/dev/kellyburton/athenaeumratingsservice/resources/RatingDataResource.java
+++ athenaeum-ratings-service/src/main/java/dev/kellyburton/athenaeumratingsservice/resources/RatingDataResource.java
@@ -1,9 +1,13 @@
 package dev.kellyburton.athenaeumratingsservice.resources;
 
 import dev.kellyburton.athenaeumratingsservice.modles.Rating;
+import dev.kellyburton.athenaeumratingsservice.modles.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -13,5 +17,17 @@ public class RatingDataResource {
     public Rating getRating(@PathVariable("bookId") String bookId ) {
         return new Rating(bookId, 5);
     }
+
+    @RequestMapping("users/{UUID}")
+    public UserRating getUserRating(@PathVariable("UUID") String UUID ) {
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
+    }
+
 
 }
